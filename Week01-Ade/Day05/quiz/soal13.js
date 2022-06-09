6e414c232f29bd16b5371a328d033633410420af
Ade <barkahadekurnia@gmail.com>
2022-06-09 09:59:09 +0700

    edit

--- Week01-Ade/Day05/quiz/soal13.js
+++ Week01-Ade/Day05/quiz/soal13.js
@@ -4,17 +4,20 @@ const findSum = (arr, weight) => {
     let num1 = 0
     let num2 = 0
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 1; j < i; j++) {
+        for (let j = i+1; j < arr.length; j++) {
             if (arr[i] + arr[j] == weight) {
-                num1 = arr[j]
-                num2 = arr[i]  
-            } 
+                num1 = arr[i]
+                num2 = arr[j]  
+                console.log(`${num1} & ${num2}`);
+                //return `${num1} & ${num2}`;
+            }             
         }
     }
-    return `${num1} & ${num2}`;
+    return `a + b = ${weight}`;
+   // return `${num1} & ${num2}`;
 }
 
-console.log(findSum([1, 2, 3, 4, 5], 9)); // 4 & 5
+console.log(findSum([1, 2, 3, 4, 5], 5)); // 4 & 5
 
 //sum biasa
 // const sum = arr.reduce((tmp,a) => tmp + a,0 ) ;
--- Week01-Ade/Day05/quiz/soal14.js
+++ Week01-Ade/Day05/quiz/soal14.js
@@ -12,8 +12,8 @@
  */
 
 function tagihanKomik(tglPinjam, tglKembali, totalkomik) {
-    var pinjam = new Date(tglPinjam)
-    var kembali = new Date(tglKembali)
+    let pinjam = new Date(tglPinjam)
+    let kembali = new Date(tglKembali)
     if (pinjam == 'Invalid Date' && kembali == 'Invalid Date') {
         return `Tgl.Pinjam ${tglPinjam} & Tgl.Kembali ${tglKembali} not valid formated date`;
     } else if (pinjam == 'Invalid Date') {
@@ -21,7 +21,7 @@ function tagihanKomik(tglPinjam, tglKembali, totalkomik) {
     } else if (kembali == 'Invalid Date') {
         return `Tgl.Kembali ${tglKembali} not valid formated date`;
     }
-    //return pinjam + kembali;
+   //return pinjam + kembali;
 
     // Calculate milliseconds in a year
     const minute = 1000 * 60
@@ -29,9 +29,20 @@ function tagihanKomik(tglPinjam, tglKembali, totalkomik) {
     const day = hour * 24
     const year = day * 365
 
-    const harga = 2500 
+    const harga = 2000  //harga sewa komik
     let hari = ( kembali.getTime() - pinjam.getTime() ) / day
-    let totalTagihan = hari * totalkomik * harga
+    let totalTagihan = hari * totalkomik * harga 
+    // lebih dari 6 hari akan dikenakan denda sebesar
+    // *  Rp.500 tiap komik. biaya komik peminjaman Rp.2000/tiap komik.
+    let denda = 500 
+    if (hari > 6 ) {
+        denda =  denda * ( hari - 6 )  
+    }
+    //return totalTagihan;
+    //return denda;
+    totalTagihan = totalTagihan + denda
+
+    //return hari;
     return `\nTotal Hari = ${hari} \nTotal Komik = ${totalkomik} x ${harga} \n------------------------ \nTotal Tagihan = Rp.${totalTagihan} \n    
     `;
 
@@ -39,7 +50,7 @@ function tagihanKomik(tglPinjam, tglKembali, totalkomik) {
  Total Hari = 3 
  Total Komik = 4 x 2500 
  ------------------------
- Total Tagihan =Rp.30000
+ Total Tagihan =Rp.24000 
  */
 }
 
@@ -48,7 +59,7 @@ console.log(tagihanKomik("09-03-20xx", "09-06-2021", 4)); //Tgl.Pinjam 09-03-20x
 console.log(tagihanKomik("09-03-2021", "09-mm-2021", 4)); //Tgl.Kembali 09-mm-2021 not valid formated date
 console.log(tagihanKomik("09-03-20xx", "09-mm-2021", 4)); //Tgl.Pinjam 09-03-20xx & Tgl.Kembali 09-mm-2021 not valid formated date
 console.log(tagihanKomik("09-03-2021", "09-06-2021", 4));
-
+console.log(tagihanKomik("09-03-2021", "09-20-2021", 4));
 
 // let days = new Date("09-03-2021") 
 
